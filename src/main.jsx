d484681098ccca631b5e96dc620cc26a6b779c9d
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-05-08 06:53:56 +0300

    bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+// import App from './App.jsx'
+// import './index.css'
+import './trivia.css'
+import LoginPage from "./pages/LoginPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <LoginPage />,
+        // errorElement: <NotFoundPage />
+    },
+    {
+        path: '/dashboard',
+        element: <DashboardPage />,
+        // errorElement: <NotFoundPage />
+    },
+    // {
+    //     path: '/profiles',
+    //     element: <ProfilesPage />,
+    //     children:[
+    //         {
+    //             path: '/profiles/:profileId',
+    //             element: <ProfilePage />
+    //         }
+    //     ]
+    // }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,34 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export default function LoginPage() {
+    return (
+        <div className="row">
+            <div className="col-4"></div>
+            <div className="col-4">
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"/>
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+            <div className="col-4"></div>
+        </div>
+
+    );
+}
