70b90cfb04d3488b66a7e392da2d453fa2b22d15
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-05-13 06:44:04 +0300

    gameboard - wip

--- src/pages/GameBoardPage.jsx
+++ src/pages/GameBoardPage.jsx
@@ -1,18 +1,22 @@
 import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect, useLayoutEffect, useState} from "react";
 import {fetchQuestions} from "../utils/http.jsx";
 
 
 export default function GameBoardPage(){
     const params = useParams()
     const [questions, setQuestions] = useState([])
+    const [currentIndex, setCurrentIndex] = useState(0)
 
     console.log(params)
     useEffect(() => {
         fetchQuestions(window.localStorage.token, params.category).then((response)=>{
-            setQuestions(response.data)
+            if(questions.length === 0){
+                console.log('1')
+                setQuestions(response.data)
+            }
         })
     }, []);
 
-    return <div>Gameboard {params.category}</div>
+    return <div>{(currentIndex+1)}. { decodeURIComponent(questions[currentIndex].question)}</div>
 }
