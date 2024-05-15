6118fbe790ae9e77b9b9f8e6c72cceea36b663c1
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-05-15 06:36:26 +0300

    fix private routes

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -2,14 +2,13 @@ import {Navigate, Outlet, useLocation} from "react-router-dom";
 
 
 export default function PrivateRoutes(){
-    // const location = useLocation();
+    const location = useLocation();
     let authLogin = window.localStorage.getItem('token') !== null;
-    console.log('authLogin:' + authLogin)
-    if (authLogin === undefined) {
+    if (authLogin === undefined || location.pathname === '/') {
         authLogin = false;
     }
 
     return authLogin
         ? <Outlet />
-        : <Navigate to="/login"  />;
+        : <Navigate to="/login" replace state={{ from: location }} />;
 }
